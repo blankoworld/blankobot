6ddfea65fceeedc6acf74ecda9ef1b860c098d74
Olivier DOSSMANN <olivier@dossmann.net>
2009-06-26 18:34:13 +0200

    Ajout de messages + pour quitter (depart) + ai renommé la fonction pour rejoindre un canal + pour partir d'un canal (ferme) + pour inviter quelqu'un (invite)

--- message_irc.rb
+++ message_irc.rb
@@ -9,6 +9,7 @@ class Message
   end
 
   def prive(dest, exp, msg)
+  # envoie un message privé (msg) à un canal (dest) ou une personne (exp)
     if dest.to_s.include?("#") or dest.to_s.include?("&")
       chaine = "PRIVMSG #{dest} :#{msg}"
     else
@@ -17,11 +18,30 @@ class Message
     return chaine
   end
 
-  def rejoindre(dest)
+  def joindre(dest)
+  # rejoint le canal donné en paramètre (dest)
     return "JOIN #{dest}"
   end
 
   def action(dest, msg)
+  # agir sur le canal donné en paramètre (dest)
     return "PRIVMSG #{dest} :ACTION #{msg}"
+  end
+
+  def depart(msg)
+  # quitte le serveur irc en laissant un message (msg)
+    msg = "Perdu dans la fracture numérique" if msg.nil?
+    return "QUIT #{msg}"
+  end
+
+  def ferme(dest, msg)
+  # ferme le canal donné en paramètre (dest)
+    msg = "" if msg.nil?
+    return "PART #{dest} :#{msg}"
+  end
+
+  def invite(dest, inv)
+  # invite une personne (inv) à rejoindre le canal (dest)
+    return "INVITE #{inv} #{dest}"
   end
 end
